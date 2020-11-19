a47c80772a9ff7fe1effd0c3e879db4fb9597fad
Nashad Rahman <22323772+nashadroid@users.noreply.github.com>
2020-11-19 00:27:59 -0500

    Update ScarletClient.m

--- ScarletClient.m
+++ ScarletClient.m
@@ -1,26 +1,26 @@
 classdef ScarletClient
     methods
         function uploadFile(obj,localFilePath, serverFilePath)
-
-            data = 'Data to send';
+            data = fileread(localFilePath);
+            contentTypeField = matlab.net.http.field.ContentTypeField('image/png'); %'image/jpeg'
+            
             body = matlab.net.http.MessageBody(data);
-            body.show
-            contentTypeField = matlab.net.http.field.ContentTypeField('text/plain'); %'image/jpeg'
+            
             type1 = matlab.net.http.MediaType('text/*');
             type2 = matlab.net.http.MediaType('application/json','q','.5');
             acceptField = matlab.net.http.field.AcceptField([type1 type2]);
             header = [acceptField contentTypeField];
             method = matlab.net.http.RequestMethod.PUT;
             request = matlab.net.http.RequestMessage(method,header,body);
-            show(request)
 
             [response,completedrequest,history] = send(request,['http://192.168.0.9:8080/', serverFilePath]);
         end
 
         function sendTextData(obj, key, val)
             data = ['{"', key , '":"', val ,'"}' ]
+
             body = matlab.net.http.MessageBody(data);
-            body.show
+            body.show;
             contentTypeField = matlab.net.http.field.ContentTypeField('text/plain'); %'image/jpeg'
             type1 = matlab.net.http.MediaType('text/*');
             type2 = matlab.net.http.MediaType('application/json','q','.5');
