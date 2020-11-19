3b943bad056fd15863c64556623c4c2ce0e71d43
Nashad <rahman.176@osu.edu>
2020-11-19 11:24:30 -0500

    cleaned up

--- testGETDict.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-requestedKEY = input("Enter the value you request:")
-
-print(requests.get('http://localhost:8080/'+ requestedKEY).text)
--- testPostImage.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-files = {'media': open('pictures/samplegraph.png', 'rb')}
-#print(files.type())
-
-requests.post('http://localhost:8080/postpictures', files=files)
