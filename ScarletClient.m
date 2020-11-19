820e81d8a6da66637a4572a5806bb754920cdf53
adamsickels00 <47608860+adamsickels00@users.noreply.github.com>
2020-11-18 23:17:17 -0500

    Update ScarletClient.m

--- ScarletClient.m
+++ ScarletClient.m
@@ -1,6 +1,6 @@
 classdef ScarletClient
     methods
-        function uploadFile(localFilePath, serverFilePath):
+        function uploadFile(obj,localFilePath, serverFilePath)
 
             data = 'Data to send';
             body = matlab.net.http.MessageBody(data);
@@ -14,14 +14,15 @@ classdef ScarletClient
             request = matlab.net.http.RequestMessage(method,header,body);
             show(request)
 
-            [response,completedrequest,history] = send(request,['http://192.168.0.9:8080/', serverFilePath])
+            [response,completedrequest,history] = send(request,['http://192.168.0.9:8080/', serverFilePath]);
+        end
 
-        function sendTextData(key, val):
+        function sendTextData(obj, key, val)
             data = ['{"', key , '":"', val ,'"}' ]
 
             body = matlab.net.http.MessageBody(data);
             body.show
-            contentTypeField = matlab.net.http.field.ContentTypeField('application/json'); %'image/jpeg'
+            contentTypeField = matlab.net.http.field.ContentTypeField('text/plain'); %'image/jpeg'
             type1 = matlab.net.http.MediaType('text/*');
             type2 = matlab.net.http.MediaType('application/json','q','.5');
             acceptField = matlab.net.http.field.AcceptField([type1 type2]);
@@ -30,17 +31,21 @@ classdef ScarletClient
             request = matlab.net.http.RequestMessage(method,header,body);
             show(request)
 
-            [response,completedrequest,history] = send(request,'http://192.168.0.9:8080/')
+            [response,completedrequest,history] = send(request,'http://192.168.0.9:8080/');
+        end
 
 
-        function data = getTextData(key):
-            request = matlab.net.http.RequestMessage()
-            [response,completedrequest,history] = send(request,['http://192.168.0.9:8080/', key])
-            data = response.Body.Data
+        function data = getTextData(obj, key)
+            request = matlab.net.http.RequestMessage();
+            [response,completedrequest,history] = send(request,['http://192.168.0.9:8080/', key]);
+            data = response.Body.Data;
+            data = convertCharsToStrings(char(data));
+        end
 
-        function data = getFile(filepath):
-            request = matlab.net.http.RequestMessage()
-            [response,completedrequest,history] = send(request,['http://192.168.0.9:8080/files/', filepath])
-            data = response.Body.Data
+        function data = getFile(obj, filepath)
+            request = matlab.net.http.RequestMessage();
+            [response,completedrequest,history] = send(request,['http://192.168.0.9:8080/files/', filepath]);
+            data = response.Body.Data;
+        end
     end
 end
